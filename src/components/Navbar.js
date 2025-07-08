d95a83ff495dde9bd59c2baaf89e1a9f3d851ed3
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-07-09 05:38:27 +0700

    new mobile navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,164 +12,112 @@ export const Navbar = () => {
 
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#fff4e0] top-0 z-50'>
-      <div>
-        <a className=' font-bold text-base uppercase text-[#052123] ' href='/'>
-          WEB DEVELOPER
+      {/* Judul */}
+      <div className='hidden md:block'>
+        <a className='font-bold text-base uppercase text-[#052123]' href='/'>
+          FULL-STACK DEVELOPER
         </a>
       </div>
-      <ul className='hidden md:flex  '>
-        <li>
-          <Link offset={-80} to='home' smooth={true} duration={500}>
-            Home
-          </Link>
-        </li>
-        <li>
-          {' '}
-          <Link offset={-80} to='about' smooth={true} duration={500}>
-            About
-          </Link>
-        </li>
 
+      {/* Desktop Menu */}
+      <ul className='hidden md:flex'>
         <li>
-          <Link offset={-80} to='education' smooth={true} duration={500}>
-            Education
-          </Link>
+          <Link offset={-80} to='home' smooth={true} duration={500}>Home</Link>
         </li>
         <li>
-          <Link offset={-80} to='experience' smooth={true} duration={500}>
-            Experience
-          </Link>
+          <Link offset={-80} to='about' smooth={true} duration={500}>About</Link>
         </li>
         <li>
-          <Link offset={-80} to='contact' smooth={true} duration={500}>
-            Contact
-          </Link>
+          <Link offset={-80} to='education' smooth={true} duration={500}>Education</Link>
+        </li>
+        <li>
+          <Link offset={-80} to='experience' smooth={true} duration={500}>Experience</Link>
+        </li>
+        <li>
+          <Link offset={-80} to='contact' smooth={true} duration={500}>Certificate</Link>
         </li>
       </ul>
 
-      {/* Hamburger */}
-      <div onClick={handleClickMenuIcon} className='md:hidden z-10'>
-        {!nav ? <FaBars /> : <FaTimes />}
+      {/* Mobile: Social icons kiri & Hamburger kanan */}
+      {/* Mobile layout: judul kiri, icon sosial tengah, hamburger kanan */}
+      <div className="md:hidden flex justify-between items-center w-full px-4">
+        {/* Judul kiri (tetap) */}
+        <div className="text-[#052123] font-bold text-sm uppercase">
+          FULL-STACK DEVELOPER
+        </div>
+
+        {/* Social icons tengah */}
+        <div className="flex gap-3 text-[#052123]">
+          <a href='https://www.linkedin.com/in/afif-syam-fauzi-365b84270/' target='_blank' rel='noopener noreferrer'>
+            <FaLinkedin size={20} />
+          </a>
+          <a href='https://www.instagram.com/affsyamf/' target='_blank' rel='noopener noreferrer'>
+            <AiFillInstagram size={20} />
+          </a>
+          <a href='https://github.com/Affsyamf' target='_blank' rel='noopener noreferrer'>
+            <FaGithub size={20} />
+          </a>
+          <a href='https://wa.me/6289517644630' target='_blank' rel='noopener noreferrer'>
+            <FaWhatsapp size={20} />
+          </a>
+        </div>
+
+        {/* Hamburger kanan */}
+        <div onClick={handleClickMenuIcon} className='text-[#052123] z-10'>
+          {!nav ? <FaBars size={20} /> : <FaTimes size={20} />}
+        </div>
       </div>
 
+
       {/* Mobile Menu */}
-      <ul
-        className={
-          !nav
-            ? 'hidden'
-            : ' absolute top-0 left-0 w-full h-screen bg-[#fff4e0] flex flex-col justify-center items-center text-[#114e53] '
-        }
-      >
+      <ul className={
+        !nav
+          ? 'hidden'
+          : 'absolute top-0 left-0 w-full h-screen bg-[#fff4e0] flex flex-col justify-center items-center text-[#114e53]'
+      }>
         <li className='py-4 text-2xl'>
-          {' '}
-          <Link
-            onClick={handleClickMenuIcon}
-            to='home'
-            smooth={true}
-            duration={500}
-            offset={-80}
-          >
-            Home
-          </Link>
+          <Link onClick={handleClickMenuIcon} to='home' smooth={true} duration={500} offset={-80}>Home</Link>
         </li>
         <li className='py-4 text-2xl'>
-          {' '}
-          <Link
-            onClick={handleClickMenuIcon}
-            to='about'
-            smooth={true}
-            duration={500}
-            offset={-80}
-          >
-            About
-          </Link>
-        </li>
-
-        <li className='py-4 text-2xl'>
-          {' '}
-          <Link
-            onClick={handleClickMenuIcon}
-            to='education'
-            smooth={true}
-            duration={500}
-            offset={-80}
-          >
-            Education
-          </Link>
+          <Link onClick={handleClickMenuIcon} to='about' smooth={true} duration={500} offset={-80}>About</Link>
         </li>
         <li className='py-4 text-2xl'>
-          {' '}
-          <Link
-            onClick={handleClickMenuIcon}
-            to='experience'
-            smooth={true}
-            duration={500}
-            offset={-80}
-          >
-            Experience
-          </Link>
+          <Link onClick={handleClickMenuIcon} to='education' smooth={true} duration={500} offset={-80}>Education</Link>
         </li>
-
         <li className='py-4 text-2xl'>
-          <Link
-            onClick={handleClickMenuIcon}
-            to='contact'
-            smooth={true}
-            duration={500}
-            offset={-80}
-          >
-            Contact
-          </Link>
+          <Link onClick={handleClickMenuIcon} to='experience' smooth={true} duration={500} offset={-80}>Experience</Link>
+        </li>
+        <li className='py-4 text-2xl'>
+          <Link onClick={handleClickMenuIcon} to='contact' smooth={true} duration={500} offset={-80}>Certificate</Link>
         </li>
       </ul>
 
-      {/* Social Icons */}
-      <div className=' hidden lg:flex fixed flex-col top-[35%] left-0 '>
+      {/* Social Icons Desktop */}
+      <div className='hidden md:flex fixed flex-col top-[35%] left-0'>
         <ul>
-          <li className='w-[100px] h-[50px] flex justify-between items-center ml-[-50px] hover:ml-[-10px] duration-300 bg-[#052123] opacity-[0.85] rounded-t-md '>
-            <a
-              href='https://www.linkedin.com/in/afif-syam-fauzi-365b84270/'
-              className='flex justify-between  items-center w-full text-[#fff4e0] '
-            >
+          <li className='w-[100px] h-[50px] flex justify-between items-center ml-[-50px] hover:ml-[-10px] duration-300 bg-[#052123] opacity-[0.85] rounded-t-md'>
+            <a href='https://www.linkedin.com/in/afif-syam-fauzi-365b84270/' className='flex justify-between items-center w-full text-[#fff4e0]'>
               LI. <FaLinkedin size={30} />
             </a>
           </li>
-          <li className='w-[100px] h-[50px] flex justify-between items-center ml-[-50px] hover:ml-[-10px] duration-300 bg-[#052123] opacity-[0.85] rounded-b-md '>
-            <a
-              href='https://www.instagram.com/affsyamf/'
-              className='flex justify-between  items-center w-full text-[#fff4e0]'
-            >
-              IG. <AiFillInstagram size={30} />{' '}
+          <li className='w-[100px] h-[50px] flex justify-between items-center ml-[-50px] hover:ml-[-10px] duration-300 bg-[#052123] opacity-[0.85]'>
+            <a href='https://www.instagram.com/affsyamf/' className='flex justify-between items-center w-full text-[#fff4e0]'>
+              IG. <AiFillInstagram size={30} />
             </a>
           </li>
-          <li className='w-[100px] h-[50px] flex justify-between items-center ml-[-50px] hover:ml-[-10px] duration-300 bg-[#052123] opacity-[0.85] rounded-b-md '>
-            <a
-              href='https://github.com/Affsyamf'
-              className='flex justify-between  items-center w-full text-[#fff4e0]'
-            >
-              GIT. <FaGithub size={30} />{' '}
+          <li className='w-[100px] h-[50px] flex justify-between items-center ml-[-50px] hover:ml-[-10px] duration-300 bg-[#052123] opacity-[0.85]'>
+            <a href='https://github.com/Affsyamf' className='flex justify-between items-center w-full text-[#fff4e0]'>
+              GIT. <FaGithub size={30} />
             </a>
           </li>
-          <li className='w-[100px] h-[50px] flex justify-between items-center ml-[-50px] hover:ml-[-10px] duration-300 bg-[#052123] opacity-[0.85] rounded-b-md '>
-            <a
-              href='https://wa.me/6289517644630'
-              className='flex justify-between  items-center w-full text-[#fff4e0]'
-              target='_blank'
-              rel="noopener noreferrer"
-            >
-              WA. <FaWhatsapp size={30} />{' '}
+          <li className='w-[100px] h-[50px] flex justify-between items-center ml-[-50px] hover:ml-[-10px] duration-300 bg-[#052123] opacity-[0.85] rounded-b-md'>
+            <a href='https://wa.me/6289517644630' className='flex justify-between items-center w-full text-[#fff4e0]' target='_blank' rel='noopener noreferrer'>
+              WA. <FaWhatsapp size={30} />
             </a>
           </li>
-          {/*  <li className='w-[100px] h-[50px] flex justify-between items-center ml-[-50px] hover:ml-[-10px] duration-300'>
-            <a
-              href='/'
-              className='flex justify-between  items-center w-full text-[#fff4e0]'
-            >
-              RE. <BsFillPersonLinesFill size={30} />{' '}
-            </a>
-          </li> */}
         </ul>
       </div>
     </div>
   );
 };
+export default Navbar;
